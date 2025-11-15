77ed0e74de80bcd7e755872e5940ebd5b31b4532
YasyaYasinskaya <chudoyasya@gmail.com>
2025-11-15 12:39:06 +0200

    highscores_loader

--- scripts/game.js
+++ scripts/game.js
@@ -4,6 +4,8 @@ const progressText = $("#progressText");
 const progressBarFull = $("#progressBarFull");
 const scoreText = $("#score");
 const scoreBonus = 10;
+const game=$("#game");
+const loader=$("#loader");
 
 
 
@@ -34,6 +36,8 @@ $.ajax({
     formattedQuestion.answerChoices = answerChoices;
     questions.push(formattedQuestion);
   }
+  game.removeClass('hidden');
+  loader.addClass('hidden');
   getNewQuestion();
 })
 .fail(function(err) {
